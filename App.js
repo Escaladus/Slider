5824e423de5b3feab7a81870185110d91f4f444e
Denis Eskelinen <escaladus@gmail.com>
2023-08-16 18:16:24 +0300

    slider

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * //Libraries
+ * npm i react-native-pager-view
+ * npm i date-fns
+ */
+
+import {
+  addDays,
+  eachDayOfInterval,
+  eachWeekOfInterval,
+  format,
+  subDays,
+} from "date-fns";
+import { StyleSheet, Text, View } from "react-native";
+import PagerView from "react-native-pager-view";
+
+const dates = eachWeekOfInterval(
+  {
+    start: subDays(new Date(), 14),
+    end: addDays(new Date(), 14),
+  },
+  {
+    weekStartsOn: 1,
+  }
+).reduce((acc, cur) => {
+  const allDays = eachDayOfInterval({
+    start: cur,
+    end: addDays(cur, 6),
+  });
+  acc.push(allDays);
+  return acc;
+}, []);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PagerView style={styles.PagerContainer}>
+      {dates.map((week, i) => {
+        return (
+          <View key={i}>
+            <View style={styles.row}>
+              {week.map((day) => {
+                const txt = format(day, "EEEEE");
+                return (
+                  <View>
+                    <Text>{txt}</Text>
+                    <Text>{day.getDate()}</Text>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
+        );
+      })}
+    </PagerView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  PagerContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
 });
